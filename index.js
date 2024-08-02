5761a031ed8e452be7998ec725ba2c6babc0cb3d
Nick Barry <itsnickbarry@protonmail.ch>
2024-08-03 02:08:25 +0300

    remove logs and comment

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const words = String(fs.readFileSync('./english.txt')).split('\n');
 const phrase = process.env.SEED_PHRASE;
 const target = getAddress(process.env.TARGET);
 
-console.log(words);
-
-//ethers.Wallet.fromPhrase
-
 for (let i = 0; i < words.length; i++) {
-  console.log(i);
   const fullPhrase = `${ phrase } ${ words[i] }`;
 
   let address;
